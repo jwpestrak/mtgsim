44eeb477a6ea54cc63af58e4f7af369004e3a3d9
James W. Pestrak <james.w.pestrak@gmail.com>
2017-12-11 19:59:43 -0800

    Add Instant class and comment next steps.

--- card.py
+++ card.py
@@ -1,36 +1,64 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+This is a module that contains Classes used for simulating draw of a hand from
+a deck of Magic: The Gathering cards.
 
-This is a temporary script file.
+Currently, in addition to a Card superclass, there are only two usable 
+subclasses - Creature and Land.
 """
+
+# Define classes
 
 class Card(object):
     
     def __init__(self, name, color):
-        self.name  = name
-        self.color = color
-        self.state = None
-    
+        self.name   = name
+        self.color  = color
+        self.state  = None # values can be 'In Hand', 'On Battlefield', 'In Graveyard', 'Exiled'
+        self.tapped = False
+        
     def set_state(self, state):
         self.state = state
     
     def get_state(self):
         return self.state
     
+    def tap(self):
+        self.tapped = True
+        
+    def untap(self):
+        self.tapped = False
+    
 class Land(Card):
     
     def __init__(self):
         pass
-    
+            
 class Creature(Card):
     
     def __init__(self, cost = {"black": 0, "blue": 0, "green": 0, "red": 0, "white": 0, "colorless": 0}):
         self.cost = cost
 
+class Instant(Card):
+    
+    def __init__(self):
+        self.tapped = None
+
+# main 
+
+# populate binder
 binder = [
     Land("Forest", "green"),
     Creature("Ancient Brontodon", "green", {"black": 0, "blue": 0, "green": 2, "red": 0, "white": 0, "colorless": 6}),
 ]
 
+# populate (and shuffle) deck
+
 #deck = [for card in binder #randomly select card until 40 are chosen]        
+
+# draw seven card hand
+
+hand = []
+
+if __name__ == "__main__":
+    pass
